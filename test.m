93773db1a271aae1b47665d3995e780dd9de65a5
Cyriac Jens <34717477+CJ1092@users.noreply.github.com>
2019-08-06 09:16:08 +0530

    testing and training

---
+++ test.m
@@ -0,0 +1,77 @@
+global exercise_id
+
+
+
+testparas = paras;
+
+testparas.dt = 0.001;
+
+
+
+% play with the parameters
+
+start_offset = [0,0];
+
+goal_offset = [0,0];
+
+speed = 1;
+
+external_force = [0,0,0,0];
+
+alpha_phaseStop = 20;
+
+
+
+
+
+testparas.y0 = trajData(1,2:end) + start_offset;
+
+testparas.dy = [0,0];
+
+testparas.goals = trajData(end,2:end) + goal_offset;
+
+testparas.tau = 1/speed;
+
+testparas.ac = alpha_phaseStop;
+
+
+
+if exercise_id == 3
+
+    testparas.extForce = external_force;
+
+else
+
+    testparas.extForce = [0,0,0,0];
+
+end
+
+
+
+
+
+testTraj = dmptest(Ws, testparas);
+
+
+
+figure
+
+axis([-2 2 -2 2]);
+
+hold on;
+
+plot(trajData(:,2), trajData(:,3), 'b-');
+
+plot(testTraj(:,2), testTraj(:,3), 'r--');
+
+
+
+figure
+
+axis([0 1 -2 2]);
+
+hold on
+
+plot(testTraj(:,1), testTraj(:,2), 'g-');
+
+plot(testTraj(:,1), testTraj(:,3), 'm-');
